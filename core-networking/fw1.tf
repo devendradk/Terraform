245ebc9c27a88d849e58cb5d8a44c10fd333a101
devendra-kulkarni-ck <devendra.kulkarni@creditkarma.com>
2021-01-05 12:33:12 -0800

    test
    test

---
+++ core-networking/fw1.tf
@@ -0,0 +1,22 @@
+resource "google_compute_firewall" "a_a_to_b" {
+  name     = "a--a-app--to--b-app"
+  network  = "default"
+  project  = "prod-test-300819"
+  priority = 950
+
+  allow {
+    protocol = "tcp"
+
+    ports = [
+      "3301-3428",
+    ]
+  }
+
+  source_tags = [
+    "a-servers",
+  ]
+
+  target_tags = [
+    "b-servers",
+  ]
+}
