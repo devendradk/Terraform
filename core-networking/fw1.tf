99f73f6f97828fe14dbd9a331893f8abbb59c811
devendradk <ddk400080@gmail.com>
2021-01-05 15:59:51 -0800

    Update fw1.tf

--- core-networking/fw1.tf
+++ core-networking/fw1.tf
@@ -2,7 +2,7 @@ resource "google_compute_firewall" "a_a_to_b" {
   name     = "a--a-app--to--b-app"
   network  = "default"
   project  = "prod-test-300819"
-  priority = 951
+  priority = 952
 
   allow {
     protocol = "tcp"
